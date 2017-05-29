ea396670675af0a93b9af63fb8f34f369f4ed580
Rodrigo <rodrigo@localhost.localdomain>
2017-05-29 09:06:16 -0300

    Se agregan test probando factores primos de 360 y 90 funcionando ok

--- factores-primos-4/spec/orden_ascendente_spec.rb
+++ factores-primos-4/spec/orden_ascendente_spec.rb
@@ -16,4 +16,18 @@ describe 'OrdenAscendete' do
     expect(valor_esperado).to match_array(valor_obtenido)
   end  
 
+  it 'factores primos de 360 deberia ser [2,2,2,3,3,5]' do
+  	valor_esperado = [2,2,2,3,3,5]
+  	factores_primos = @calculadora.calcular_factores_primos(360)
+  	valor_obtenido = @orden_ascendente.aplicar_orden(factores_primos)
+    expect(valor_esperado).to match_array(valor_obtenido)
+  end 
+
+  it 'factores primos de 90 deberia ser [2,3,3,5]' do
+  	valor_esperado = [2,3,3,5]
+  	factores_primos = @calculadora.calcular_factores_primos(90)
+  	valor_obtenido = @orden_ascendente.aplicar_orden(factores_primos)
+    expect(valor_esperado).to match_array(valor_obtenido)
+  end 
+
 end
