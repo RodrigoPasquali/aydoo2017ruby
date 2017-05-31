e96938b17c112f24c6a2d548eed503dfb9407037
Rodrigo <rodrigo@localhost.localdomain>
2017-05-31 08:31:22 -0300

    Refactor y se agregan test a salida_spec

--- factores-primos-4/model/salida.rb
+++ factores-primos-4/model/salida.rb
@@ -4,48 +4,33 @@ require_relative 'orden_ascendente'
 require_relative 'orden_descendente'
 require_relative 'calculadora'
 
-class Salida 
+class Salida
+  attr_accessor :calculadora, :verificador, :formateador, :ordenador_ascendente, :ordenador_descendente
 
-  def salida_orden_ascendente(numero_a_factorizar)
-  	@calculdora = Calculadora.new
-    @verificador = VerificadorDeArgumentos.new
-    @formateador = FormatoConComas.new
-    @orden_ascendente = OrdenAscendente.new
-    @ordenador_descendente = OrdenDescendente.new
-    @salida_obtenida
-    if @verificador.verificar_si_es_numero(numero_a_factorizar) == true
-      if @verificador.verificar_si_numero_es_mayor_a_cero(numero_a_factorizar) == true
-        factores_primos = @calculdora.calcular_factores_primos(numero_a_factorizar)
-        factores_ordenados = @orden_ascendente.aplicar_orden(factores_primos)
-        factores_ordenados_con_formato = @formateador.aplicar_formato_con_comas(factores_ordenados)
-      	@salida_obtenida = factores_ordenados_con_formato
-      else
-      	"Debe ingresar un numero mayor a 0"
-      end
-    else
-      "Debe ingresar un numero"
-    end
+  def inicializar
+  	self.calculadora = Calculadora.new
+    self.verificador = VerificadorDeArgumentos.new
+    self.formateador = FormatoConComas.new
+    self.ordenador_ascendente = OrdenAscendente.new
+    self.ordenador_descendente = OrdenDescendente.new
   end
 
-    def salida_orden_ascendente(numero_a_factorizar)
-  	@calculdora = Calculadora.new
-    @verificador = VerificadorDeArgumentos.new
-    @formateador = FormatoConComas.new
-    @orden_ascendente = OrdenAscendente.new
-    @ordenador_descendente = OrdenDescendente.new
-    @salida_obtenida
-    if @verificador.verificar_si_es_numero(numero_a_factorizar) == true
-      if @verificador.verificar_si_numero_es_mayor_a_cero(numero_a_factorizar) == true
-        factores_primos = @calculdora.calcular_factores_primos(numero_a_factorizar)
-        factores_ordenados = @orden_ascendente.aplicar_orden(factores_primos)
-        factores_ordenados_con_formato = @formateador.aplicar_formato_con_comas(factores_ordenados)
-      	@salida_obtenida = factores_ordenados_con_formato
-      else
-      	"Debe ingresar un numero mayor a 0"
-      end
-    else
-      "Debe ingresar un numero"
-    end
+  def salida_orden_ascendente(numero_a_factorizar)
+    @verificador.verificar_si_es_numero(numero_a_factorizar)
+    @verificador.verificar_si_numero_es_mayor_a_cero(numero_a_factorizar)
+    @factores_primos = @calculadora.calcular_factores_primos(numero_a_factorizar)
+    @factores_ordenados = @ordenador_ascendente.aplicar_orden(@factores_primos)
+    @factores_ordenados_con_formato = @formateador.aplicar_formato_con_comas(@factores_ordenados)
+    @salida_obtenida = @factores_ordenados_con_formato
+  end
+
+  def salida_orden_descendente(numero_a_factorizar)
+    @verificador.verificar_si_es_numero(numero_a_factorizar)
+    @verificador.verificar_si_numero_es_mayor_a_cero(numero_a_factorizar)
+    @factores_primos = @calculadora.calcular_factores_primos(numero_a_factorizar)
+    @factores_ordenados = @ordenador_descendente.aplicar_orden(@factores_primos)
+    @factores_ordenados_con_formato = @formateador.aplicar_formato_con_comas(@factores_ordenados)
+    @salida_obtenida = @factores_ordenados_con_formato
   end
 
 end
--- factores-primos-4/spec/salida_spec.rb
+++ factores-primos-4/spec/salida_spec.rb
@@ -5,6 +5,7 @@ describe 'Salida' do
 
   before do
     @salida = Salida.new
+    @salida.inicializar
   end
 
   it 'factores primos de 10 deberia devolver 2,5' do
@@ -18,5 +19,17 @@ describe 'Salida' do
   	valor_obtenido = @salida.salida_orden_ascendente(360)
     expect(valor_obtenido).to eq valor_esperado
   end 
+
+  it 'factores primos de 10 deberia devolver 5,2' do
+  	valor_esperado = "5,2"
+  	valor_obtenido = @salida.salida_orden_descendente(10)
+    expect(valor_obtenido).to eq valor_esperado
+  end  
+
+  it 'factores primos de 360 deberia devolver 5,3,3,2,2,2' do
+  	valor_esperado = "5,3,3,2,2,2"
+  	valor_obtenido = @salida.salida_orden_descendente(360)
+    expect(valor_obtenido).to eq valor_esperado
+  end
 
 end
